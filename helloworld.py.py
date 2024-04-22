a2a119719f603601d348ae8517945e2dd48d3219
Milli87x <Milliyongebrehiwot870@gmail.com>
2024-04-22 06:03:49 -1200

    Initial commit

---
+++ helloworld.py.py
@@ -0,0 +1,57 @@
+'''day 1 exercise {read all the way to day eight tho :-) } 
+Check the python version you are using
+Open the python interactive shell and do the following operations. The operands are 3 and 4.
+ addition(+)
+ subtraction(-)
+ multiplication(*)
+ modulus(%)
+ division(/)
+ exponential(**)
+ floor division operator(//)
+ Write strings on the python interactive shell. The strings are the following:
+ Your name
+ Your family name
+ Your country
+ I am enjoying 30 days of python
+ Check the data types of the following data:
+ 10
+ 9.8
+ 3.14
+ 4 - 4j
+ 
+ Your name
+ Your family name
+ Your country'''
+
+# 1 to check python version we need to open bash and type python --version
+# 2 to open pyt interactve shell we need to open a terminal amd type python or python3
+#   this allows us to run any mathematical operation
+#we dont really need to do this bc pthon does this too, Example
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+# to check data types we can use print(type())
+name="milliyon"
+family_name="gebrehiowt"
+country="Ethiopia"
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(name))
+print(type(family_name))
+print(type(country))
+
+
+
+
+
+
+
+
+
+
